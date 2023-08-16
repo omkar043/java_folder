80406d77c45e993351813a0b22758acee034c5d1
Your Name <you@example.com>
2023-08-16 19:21:44 +0530

    omkar : 2nd commit(Some changes in code)

--- data_structure/src/github/Program_ex.java
+++ data_structure/src/github/Program_ex.java
@@ -4,7 +4,9 @@ public class Program_ex {
 
 	public static void main(String[] args) {
 		System.out.println("hello world !!!!");
-
+		String name = "Omkar";
+		System.out.println("hiii my name is "+name);
+       
 	}
 
 }
